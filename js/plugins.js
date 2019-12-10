c6b26bc570e42121f0a1bb37ade003204e0e5c4f
motogshit <54294190+motogshit@users.noreply.github.com>
2019-12-10 18:38:01 -0400

    A
    A

--- js/plugins.js
+++ js/plugins.js
@@ -128,3 +128,60 @@ $(document).ready(function(){ //when document(DOM) loads completely
         }
     });
   });
+
+  /*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE ==========*/
+$(document).ready(function () {
+    if ($(window).width() < 768) {
+        $('div').attr('data-animation', 'fadeInUp');
+    }
+});
+
+/*========== WAYPOINTS ANIMATION DELAY ==========*/
+//Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
+$(function () { // a self calling function
+    function onScrollInit(items, trigger) { // a custom made function
+        items.each(function () { //for every element in items run function
+            var osElement = $(this), //set osElement to the current
+                osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
+                osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
+  
+            osElement.css({ //change css of element
+                '-webkit-animation-delay': osAnimationDelay, //for safari browsers
+                '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
+                'animation-delay': osAnimationDelay //normal
+            });
+  
+            var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+  
+            osTrigger.waypoint(function () { //scroll upwards and downwards
+                osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
+            }, {
+                    triggerOnce: true, //only once this animation should happen
+                    offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
+                });
+        });
+    }
+  
+    onScrollInit($('.os-animation')); //function call with only items
+    onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+  });
+
+  /*========== SLIDER DOS PARCEIROS ==========*/
+$(document).ready(function(){ //when document(DOM) loads completely
+    $('#clients-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1600, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //768px width and up display 2 items
+            }
+        }
+    });
+  });
